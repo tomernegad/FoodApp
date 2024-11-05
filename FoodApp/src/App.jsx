488f74569a607ebba9b2d16400ea2ac9b2156f4e
tomernegad <tomernegad@gmail.com>
2024-11-05 11:51:29 +0200

    Added inner and outer containers to arrange the components

--- FoodApp/src/App.jsx
+++ FoodApp/src/App.jsx
@@ -3,13 +3,19 @@ import Search from "./components/Search";
 import FoodList from "./components/FoodList";
 import Nav from "./components/Nav";
 import "./App.css";
+import Container from "./components/Container";
+import InnerContainer from "./components/InnerContainer";
 function App() {
   const [foodData, setFoodData] = useState([]);
   return (
     <div className="App">
       <Nav />
       <Search foodData={foodData} setFoodData={setFoodData} />{" "}
-      <FoodList foodData={foodData} />
+      <Container>
+        <InnerContainer>
+          <FoodList foodData={foodData} />
+        </InnerContainer>
+      </Container>
     </div>
   );
 }
