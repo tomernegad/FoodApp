b1450ee0dcc75a971fad08a5e45db0dde698a020
tomernegad <tomernegad@gmail.com>
2024-11-07 13:03:15 +0200

    Styling items container

--- FoodApp/src/components/Item.jsx
+++ FoodApp/src/components/Item.jsx
@@ -1,14 +1,22 @@
+import styles from "./item.module.css";
 export default function Item({ item }) {
   return (
     <div>
-      <div>
-        <img
-          src={`https://spoonacular.com/cdn/ingredients_100x100/` + item.image}
-        />
-        <h3>{item.name}</h3>
-        <h3>
-          {item.amount} {item.unit}
-        </h3>
+      <div className={styles.itemContainer}>
+        <div className={styles.imageContainer}>
+          <img
+            className={styles.image}
+            src={
+              `https://spoonacular.com/cdn/ingredients_100x100/` + item.image
+            }
+          />
+        </div>
+        <div className={styles.nameContainer}>
+          <div className={styles.name}>{item.name}</div>
+          <div className={styles.amount}>
+            {item.amount} {item.unit}
+          </div>
+        </div>
       </div>
     </div>
   );
